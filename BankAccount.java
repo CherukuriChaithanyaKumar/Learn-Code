2774d77627a2f2143dd95982c215ab2d23b2b62f
CherukuriChaithanyaKumar <cherukuri.c.k@intimetec.com>
2025-04-03 19:30:02 +0530

    Refactor

--- BankAccount.java
+++ BankAccount.java
@@ -1,7 +1,6 @@
 package oops;
 abstract class BankAccount {
 	private String accountHolder;
-	private String accountBalance;
 	private double balance;
 
 	public BankAccount(String accountHolder, double balance) {
@@ -13,7 +12,6 @@ abstract class BankAccount {
 		return balance;
 	}
 
-	// Abstract method to be implemented by subclasses
 	public void deposit(double amount) {
 	}
 
